5ea67e5d4eee61a9cf1a5c413e14971d6c808cf6
Atharva Bari <atharvabari0708@gmail.com>
2026-05-18 01:38:13 +0530

    Lecture 1

---
+++ Lecture 1/Input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+print("Welcome, ", name)
+
+
+value = int(input("Enter a no: "))
+print(type(value), value)
---
+++ Lecture 1/Type.py
@@ -0,0 +1,8 @@
+# type conversion
+a = 4
+b = 2.123
+print(a + b)
+
+a = int("4")
+b = 2.123
+print(a + b)
