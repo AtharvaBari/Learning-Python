c4fe0785f0d0c2d5e956142d7b5db3bb4a3d167b
Atharva Bari <atharvabari0708@gmail.com>
2026-05-18 01:24:39 +0530

    Lecture 1

---
+++ Lecture 1/Assignment.py
@@ -0,0 +1,15 @@
+num = 10
+num += 10 # Same as num = num + 10
+print(num)
+
+num = 10
+num -= 10 # Same as num = num - 10
+print(num)
+
+num = 10
+num *= 10 # Same as num = num * 10
+print(num)
+
+num = 10
+num /= 2 # Same as num = num / 10
+print(num)
