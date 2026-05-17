96a44b161043427f0d7e840688eafc655cccd58f
Atharva Bari <atharvabari0708@gmail.com>
2026-05-17 20:49:25 +0530

    New Try

---
+++ Lecture 1/DataTypes.py
@@ -0,0 +1,11 @@
+name = "atharva"
+age = 18
+dob_in_decimal = 16.07
+adult = True
+other = None
+
+print(type(name))
+print(type(age))
+print(type(dob_in_decimal))
+print(type(adult))
+print(type(other))
