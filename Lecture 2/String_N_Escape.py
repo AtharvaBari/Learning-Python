5ff6072a0e8917be092ad0a2f63799dafcc898f1
Atharva Bari <atharvabari0708@gmail.com>
2026-05-18 04:00:44 +0530

    Lecture 1

---
+++ Lecture 2/String_N_Escape.py
@@ -0,0 +1,12 @@
+# We can add string using 3 types 
+
+str1 = "Hello World"
+str2 = 'Hello World'
+str3 = """Hello World"""
+
+# There are escape sequences in code to form the string view 
+
+str0 = "Hello Atharva,\nThis is a Joining Letter"
+print(str0)
+str00 = "Hello Atharva,\tThis is a Joining Letter"
+print(str00)
