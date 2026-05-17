1129ec03d8a5fb3890a6ed974bb14f9043c4be42
Atharva Bari <atharvabari0708@gmail.com>
2026-05-18 01:30:51 +0530

    Lecture 1

--- Lecture 1/Assignment.py
+++ Lecture 1/Assignment.py
@@ -13,3 +13,7 @@ print(num)
 num = 10
 num /= 2 # Same as num = num / 10
 print(num)
+
+num = 10
+num %= 2 # Same as num = num % 10
+print(num)
---
+++ Lecture 1/Logical.py
@@ -0,0 +1,30 @@
+print(not False)
+print(not True)
+
+a = 50
+b = 70
+print(not (a < b))
+
+print("-------------------------------------")
+
+val1 = True
+val2 = True
+print("and OP: ", val1 and val2)
+print("or OP: ", val1 or val2)
+
+val1 = True
+val2 = False
+print("and OP: ", val1 and val2)
+print("or OP: ", val1 or val2)
+
+val1 = False
+val2 = True
+print("and OP: ", val1 and val2)
+print("or OP: ", val1 or val2)
+
+val1 = False
+val2 = False
+print("and OP: ", val1 and val2)
+print("or OP: ", val1 or val2)
+
+print("-------------------------------------")
