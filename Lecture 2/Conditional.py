ecbb5147c2837c33e2c352b422be7fd2ac5164ce
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 01:53:58 +0530

    All Commmit

---
+++ Lecture 2/Conditional.py
@@ -0,0 +1,10 @@
+grade = 75
+
+if (grade >= 90):
+    print("Grade: A+")
+elif (grade >= 80 and grade < 90):
+    print("Grade: B")
+elif (grade >=70 and grade < 80):
+    print("Grade: C")
+else:
+    print("Grade: D")
