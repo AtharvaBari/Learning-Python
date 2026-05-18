0523b37a9c5991aefd65fd87bf3d30289d019b02
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 02:09:51 +0530

    All Commmit

---
+++ Lecture 2/Practice4.py
@@ -0,0 +1,22 @@
+a = 20
+b = 15
+c = 10
+d = 5
+
+if (a >= b and a >= c and a >= d):
+    print("Largest no. is a = ", a)
+elif (b >= a and b >= c and b >= d):
+    print("Largest no. is b = ", b)
+elif (c >= a and c >= b and c >= d):
+    print("Largest no. is c = ", c)
+else:
+    print("Largest no. is d = ", d)
+
+    
+# if (a >= b and a >= c and a >= d): 
+#     print("Largest no. is a = ", a) 
+# elif (b >= c and b >= d): 
+#     print("Largest no. is b = ", b) 
+# elif (c >= d): 
+#     print("Largest no. is c = ", c)
+# else: print("Largest no. is d = ", d)
