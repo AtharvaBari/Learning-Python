92fa6b580159a995647b4ee49e9b043b0edd73b5
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 04:13:58 +0530

    New Try

---
+++ Lecture 3/List_Methods.py
@@ -0,0 +1,17 @@
+list = [8, 9, 1, 2, 5]
+
+list.append(6)
+print(list)
+
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
+
+list2 = [35, 64, 37, 94, 25]
+list2.reverse()
+print(list2)
+
+list2.insert(2, 888)
+print(list2)
