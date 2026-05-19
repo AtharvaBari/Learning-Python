7c7c656ca78701328018097f2a2458cde98663fc
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 11:24:46 +0530

    New Try

---
+++ Lecture 4/Dictionary.py
@@ -0,0 +1,16 @@
+dict = {
+    "Name" : "Atharva Bari",
+    "Roll no." : 5,
+    "Course" : "TYCO",
+    "Subjects" : ["DCN", "JPR", "MIC", "OOP"],
+    "Marks" : {
+        "DCN" : 45,
+        "JPR" : 46,
+        "MIC" : 47,
+        "OOP" : 48
+    }
+}
+
+print(dict, "\n")
+print(dict["Subjects"], "\n")
+print(dict["Marks"]["DCN"])
