721b735ee2e0c23fda72162fbc6f351327e48c7c
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 12:21:02 +0530

    All Commit

---
+++ Lecture 4/Practice3.py
@@ -0,0 +1,9 @@
+dict = {}
+
+JPR = input("Enter JPR MARKS: ")
+DCN = input("Enter DCN MARKS: ")
+MIC = input("Enter MIC MARKS: ")
+
+dict.update({"JPR" : JPR, "DCN" : DCN, "MIC": MIC } )
+
+print(dict)
---
+++ Lecture 4/Practice4.py
@@ -0,0 +1,4 @@
+set1 = {("Int", 9),
+        ("Float", 9.0)}
+
+print(set1)
