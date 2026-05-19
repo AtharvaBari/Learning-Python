309747073e24e6954f31191f41276ba60c1c2f9a
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 11:54:15 +0530

    All Commit

---
+++ Lecture 4/Sets.py
@@ -0,0 +1,8 @@
+set1 = { 1, 2, 4, 4, 4, "Atharva", "Bari", "Bari"}
+
+print(set1)
+print(len(set1))
+
+null_set = set()
+print(null_set)
+print(type(null_set))
---
+++ Overview/1.py
@@ -0,0 +1,14 @@
+# List are mutable 
+list = [1, 2, 3, 4]
+
+#Tuple are immutable
+tuple = (1, 2, 3, 4)
+
+# Dictionary is mutable 
+dict = {
+    "key" : "value"
+} 
+
+# Sets are immutable 
+
+set = {1, 2, 3, 4}
