0a9faec69b3883fcb031d47cfb41346a0d6f2d1f
Atharva Bari <atharvabari0708@gmail.com>
2026-05-19 12:13:13 +0530

    All Commit

---
+++ Lecture 4/Practice1.py
@@ -0,0 +1,6 @@
+dict = {
+    "table" : ("a piece of furniture", "list of facts and figures"),
+    "cat" : "A Small animal"
+}
+
+print(dict)
---
+++ Lecture 4/Practice2.py
@@ -0,0 +1,5 @@
+list_set = {"Python", "Java", "C++", "Python", "JavaScript", "Java", "Python", "Java", "C++", "C"}
+
+classrooms_required = (len(list_set))
+
+print(classrooms_required)
