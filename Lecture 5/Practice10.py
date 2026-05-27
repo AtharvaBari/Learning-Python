cbf7995d851590c71ef71fb1a4991ac19861adc3
Atharva Bari <atharvabari0708@gmail.com>
2026-05-28 02:13:36 +0530

    All Commit

---
+++ Lecture 5/Practice10.py
@@ -0,0 +1,14 @@
+x = 5
+
+for i in range(0, (x*10)+1, x):
+    print(i)
+
+print("\n") 
+
+for i in range(0, x*11, x):
+    print(i)
+
+print("\n") 
+
+for i in range(1, 11):
+    print(x * i)
---
+++ Lecture 5/Range.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+
+print("\n")
+
+for i in range(2, 6):
+    print(i)
+
+print("\n")
+
+for i in range(1, 20, 2):
+    print(i)
