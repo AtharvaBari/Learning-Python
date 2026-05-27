f5500ebab3f34f3cbba52548a734f9d766230d29
Atharva Bari <atharvabari0708@gmail.com>
2026-05-28 02:27:38 +0530

    All Commit

---
+++ Lecture 5/Practice11.py
@@ -0,0 +1,21 @@
+# x = 10
+# i = 1
+# a = 0
+# sum = 0
+# while (i <= x):
+#     a = a + 1
+#     sum = sum + a
+#     i += 1
+
+# print(sum)
+
+
+
+n = 10
+i = 0
+sum = 0
+
+for i in range(1, n+1):
+    sum += i
+    
+print(sum)
---
+++ Lecture 5/Practice12.py
@@ -0,0 +1,18 @@
+# x = 10
+# prod = 1
+
+# for i in range(x, 0, -1):
+#     prod = prod * i
+
+# print(prod)
+
+
+x = 10
+prod = 1
+i = 1
+
+while i <= x:
+    prod = prod * i
+    i += 1
+
+print(prod)
