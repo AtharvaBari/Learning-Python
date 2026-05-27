1bf64590d09e08c063d84ec5d4fbf4e8d155e651
Atharva Bari <atharvabari0708@gmail.com>
2026-05-28 03:49:55 +0530

    All Commit

---
+++ Lecture 6/Practice4.py
@@ -0,0 +1,12 @@
+# def inr(usd):
+#     amt = usd * 95
+#     return amt
+
+# print(inr(20))
+
+
+def inr(usd):
+    inr = usd * 96
+    print(usd, "Dollar =", inr, "Rupees")
+
+inr(20)
---
+++ Lecture 6/PracticeHW.py
@@ -0,0 +1,9 @@
+def check(x):
+    if (x % 2 == 0):
+        print("even no")
+    elif (x % 2 == 1):
+        print("odd no")
+    else:
+        print("Something invalid")
+
+check(100)
